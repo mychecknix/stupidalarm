bc54c677dde2e3994a11247b1a95ec3bbbde720b
Bernd Stellwag <burned@zerties.org>
2017-01-20 22:02:19 +0100

    include game.js in index.html

--- src/game.js
+++ src/game.js
@@ -1,15 +1,13 @@
-window.onload = function () {
-    var game = Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
-    
-    function preload() {
-        
-    }
-    
-    function create() {
-        
-    }
-    
-    function update() {
-        
-    }
-};
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
