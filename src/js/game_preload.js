dd9b86541044ed24b585019de948ac51e954a106
Jan <ggj@nemz.org>
2017-01-21 17:56:27 +0100

    add Preload State

---
+++ src/js/game_preload.js
@@ -0,0 +1,17 @@
+var gamePreload = function(game) {}
+
+gamePreload.prototype = {
+    preload: function() {
+        var loadingBar = this.add.sprite(160, 240, "loading");
+        loadingBar.anchor.setTo(0.5, 0.5);
+        this.load.setPreloadSprite(loadingBar);
+
+        this.game.load.image('dummy', 'assets/dummy.png');
+        this.game.load.spritesheet('character', 'assets/character_sprite.png', 16, 32, 8);
+        this.game.load.audio('alarm', 'assets/Kevin_MacLeod_-_Monkeys_Spinning_Monkeys.mp3');
+        this.game.load.image('tiles', 'assets/tiles.png');
+    },
+    create: function() {
+        this.game.start.state("Menu");
+    }
+};
