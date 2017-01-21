3070d362ec691df388853ab576267390b5df7d3b
Jan <ggj@nemz.org>
2017-01-21 20:59:13 +0100

    confirm game menu with spacebar

--- src/js/game_menu.js
+++ src/js/game_menu.js
@@ -4,6 +4,11 @@ gameMenu.prototype = {
     create: function() {
         var playGame = this.game.add.button(160, 320, 'play', this.playGame, this);
         playGame.anchor.setTo(0.5, 0.5);
+        space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    },
+    update: function() {
+        if (space.isDown)
+            this.playGame();
     },
     playGame: function() {
         this.game.state.start("StupidAlarm");
