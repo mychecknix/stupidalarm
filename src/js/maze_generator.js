a5868ac2549f8eb0f7257ddbe392f8b1fb9a0798
Bernd Stellwag <burned@zerties.org>
2017-01-21 10:20:23 +0100

    first working version

--- src/js/maze_generator.js
+++ src/js/maze_generator.js
@@ -1,7 +1,13 @@
-var MAZE_WALL = 'wall';
-var MAZE_FLOOR = 'floor';
-var MAZE_UNVISITED = 'unvisited';
+var MAZE_WALL = '*';
+var MAZE_FLOOR = '.';
 
+/**
+ * generates a maze
+ *
+ * @param width
+ * @param height
+ * @returns {Array}
+ */
 function maze_generator(width, height) {
     // Randomized Prim's algorithm
     // see https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Prim.27s_algorithm
@@ -10,53 +16,101 @@ function maze_generator(width, height) {
 
     // Step 1: Start with a grid full of walls.
     for (var i = 0; i < width; i++) {
+        if (typeof maze[i] === 'undefined') {
+            maze[i] = [];
+        }
         for (var j = 0; j < height; j++) {
-            maze[i][j] = MAZE_UNVISITED;
+            maze[i][j] = MAZE_WALL;
         }
     }
 
     // Step 2: Pick a cell, mark it as part of the maze. Add the walls of the cell to the wall list.
-    var cellX = get_random_int(0, width - 1);
-    var cellY = get_random_int(0, height - 1);
-    maze[cellX][cellY] = MAZE_FLOOR;
+    var startCellX = get_random_int(0, width - 1);
+    var startCellY = get_random_int(0, height - 1);
+    // startCellX = 0;
+    // startCellY = 0;
+    maze[startCellX][startCellY] = MAZE_FLOOR;
 
-    // wall to the right (x + 1, y)
-    if (typeof maze[cellX + 1][cellY] !== 'undefined' && maze[cellX + 1][cellY] === 1) {
-        walls.push([[cellX, cellY], [cellX + 1, cellY]]);
-    }
-    // wall to the left (x - 1, y)
-    else if (typeof maze[cellX - 1][cellY] !== 'undefined' && maze[cellX - 1][cellY] === 1) {
-        walls.push([[cellX, cellY], [cellX - 1, cellY]]);
-    }
-    // wall to the top (x, y + 1)
-    else if (typeof maze[cellX][cellY + 1] !== 'undefined' && maze[cellX][cellY + 1] === 1) {
-        walls.push([[cellX, cellY], [cellX, cellY + 1]]);
-    }
-    // wall to the bottom (x, y - 1)
-    else if (typeof maze[cellX][cellY - 1] !== 'undefined' && maze[cellX][cellY - 1] === 1) {
-        walls.push([[cellX, cellY], [cellX, cellY - 1]]);
+    for (i = -1; i <= 1; i++) {
+        if (typeof maze[startCellX + i] !== 'undefined') {
+            for (j = -1; j <= 1; j++) {
+                if (
+                    (i === 0 && j === 0)
+                    || (i !== 0 && j !== 0)
+                    || typeof maze[startCellX + i][startCellY + j] === 'undefined'
+                    || maze[startCellX + i][startCellY + j] === MAZE_FLOOR
+                ) {
+                    continue;
+                }
+
+                walls.push([[startCellX, startCellY], [startCellX + i, startCellY + j]]);
+            }
+        }
     }
 
     // Step 3: While there are walls in the list
+    var lastCellX, lastCellY;
     while (walls.length > 0) {
         // Step 3.1: Pick a random wall from the list. If only one of the two cells that the wall divides is visited, then
         var wall_id = get_random_int(0, walls.length - 1);
         var wall = walls[wall_id];
 
-        var cellA = maze[wall[0][0]][wall[0][1]];
-        var cellB = maze[wall[1][0]][wall[1][1]];
-        if (
-            (cellA === MAZE_UNVISITED && cellB !== MAZE_UNVISITED)
-            || (cellA !== MAZE_UNVISITED && cellA === MAZE_UNVISITED)
-        ) {
-            // Step 3.1.1: Make the wall a passage and mark the unvisited cell as part of the maze
-            // Step 3.1.2: Add the neighboring walls of the cell to the wall list
+        var cellFloorX = wall[0][0];
+        var cellFloorY = wall[0][1];
+        var cellWallX = wall[1][0];
+        var cellWallY = wall[1][1];
+
+        var cellOppositeX, cellOppositeY;
+        if (cellFloorX === cellWallX) {
+            cellOppositeX = cellFloorX;
+            cellOppositeY = cellWallY + (cellWallY - cellFloorY);
+        }
+        else if (cellFloorY === cellWallY) {
+            cellOppositeX = cellWallX + (cellWallX - cellFloorX);
+            cellOppositeY = cellFloorY;
+        }
+
+        if (typeof maze[cellOppositeX] !== 'undefined' && typeof maze[cellOppositeX][cellOppositeY] !== 'undefined') {
+            var cellOpposite = maze[cellOppositeX][cellOppositeY];
+            if (
+                (maze[cellFloorX][cellFloorY] === MAZE_FLOOR && cellOpposite === MAZE_WALL)
+                || (maze[cellFloorX][cellFloorY] === MAZE_WALL && cellOpposite === MAZE_FLOOR)
+            ) {
+                // Step 3.1.1: Make the wall a passage and mark the unvisited cell as part of the maze
+                maze[cellWallX][cellWallY] = MAZE_FLOOR;
+                maze[cellOppositeX][cellOppositeY] = MAZE_FLOOR;
+                lastCellX = cellOppositeX;
+                lastCellY = cellOppositeY;
+
+                // Step 3.1.2: Add the neighboring walls of the cell to the wall list
+                for (i = -1; i <= 1; i++) {
+                    if (typeof maze[cellOppositeX + i] !== 'undefined') {
+                        for (j = -1; j <= 1; j++) {
+                            if (
+                                (i === 0 && j === 0)
+                                || (i !== 0 && j !== 0)
+                                || typeof maze[cellOppositeX + i][cellOppositeY + j] === 'undefined'
+                                || maze[cellOppositeX + i][cellOppositeY + j] === MAZE_FLOOR
+                            ) {
+                                continue;
+                            }
+
+                            walls.push([[cellOppositeX, cellOppositeY], [cellOppositeX + i, cellOppositeY + j]]);
+                        }
+                    }
+                }
+            }
         }
 
         // Step 3.2: Remove the wall from the list
+        walls.splice(wall_id, 1);
     }
 
-    return maze;
+    return {
+        startCell: [startCellX, startCellY],
+        lastCell: [lastCellX, lastCellY],
+        maze: maze
+    };
 }
 
 /**
