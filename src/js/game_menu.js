68a46b20d966e326ba0746d715f380331b2ff946
gruebmich <michaelbernhardgrueb@web.de>
2017-01-21 21:47:58 +0100

    make menu nicer

--- src/js/game_menu.js
+++ src/js/game_menu.js
@@ -2,6 +2,16 @@ var gameMenu = function(game) {};
 
 gameMenu.prototype = {
     create: function() {
+        var playGame = this.game.add.button(this.game.width * 0.1, this.game.height * 0.6, 'play', this.playGame, this);
+        //wall
+        var tilepart1 = this.game.add.sprite(this.game.width - 32, this.game.height - 32, 'tiles', 1);
+        //floor
+        var tilepart2 = this.game.add.sprite(this.game.width - 64, this.game.height - 32, 'tiles', 0);
+        //clock
+        var tilepart3 = this.game.add.sprite(this.game.width - 32, this.game.height - 64, 'tiles', 2);
+        //character
+        var tilepart4 = this.game.add.sprite(this.game.width * 0.7, this.game.height * 0.2, 'character', 0);
+        tilepart4.scale.setTo(10, 10);
         var playGame = this.game.add.button(160, 320, 'play', this.playGame, this);
         playGame.anchor.setTo(0.5, 0.5);
         space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
--- src/js/game_preload.js
+++ src/js/game_preload.js
@@ -9,7 +9,7 @@ gamePreload.prototype = {
         this.game.load.image("dummy", "assets/dummy.png");
         this.game.load.spritesheet("character", "assets/character_sprite.png", 16, 32, 8);
         this.game.load.audio("alarm", "assets/Kevin_MacLeod_-_Monkeys_Spinning_Monkeys.mp3");
-        this.game.load.image("tiles", "assets/tiles.png");
+        this.game.load.spritesheet("tiles", "assets/tiles.png", 32, 32, 3);
         this.game.load.image("play", "assets/play.png");
     },
     create: function() {
