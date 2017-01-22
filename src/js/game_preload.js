e85b7f3f80ab3f19c7c6fe45f806687c9cf6c5cf
Bernd Stellwag <burned@zerties.org>
2017-01-22 13:48:44 +0100

    remove unused files

--- src/js/game_preload.js
+++ src/js/game_preload.js
@@ -7,7 +7,6 @@ gamePreload.prototype = {
         loadingBar.anchor.setTo(0.5, 0.5);
         this.load.setPreloadSprite(loadingBar);
 
-        this.game.load.image("dummy", "assets/dummy.png");
         this.game.load.spritesheet("character", "assets/character_sprite.png", 16, 32, 8);
         this.game.load.audio("alarm", "assets/Kevin_MacLeod_-_Monkeys_Spinning_Monkeys.mp3");
         this.game.load.spritesheet("tiles", "assets/tiles.png", 32, 32, 3);
