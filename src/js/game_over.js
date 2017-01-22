512b9b7412becd0e27e2fb48640e34a590c172cc
gruebmich <michaelbernhardgrueb@web.de>
2017-01-22 12:50:12 +0100

    added time display

--- src/js/game_over.js
+++ src/js/game_over.js
@@ -3,8 +3,8 @@ var gameOver = function (game) {
 
 gameOver.prototype = {
     init: function (time) {
-        // TODO make something nicer
-
+        time = time + " sec.";
+        this.game.add.text(this.game.width * 0.7, this.game.height * 0.9, score, {font: '24px Arial', fill: '#ffffff'});
     },
     create: function() {
         var endGame = this.game.add.emitter(this.game.world.centerX, this.world.centerY, 10);
@@ -18,7 +18,7 @@ gameOver.prototype = {
         game.add.tween(endGame).to( { emitX: 800-64 }, 1000, Phaser.Easing.Sinusoidal.InOut, true, 0, Number.MAX_VALUE, true);
         game.add.tween(endGame).to( { emitY: 200 }, 4000, Phaser.Easing.Sinusoidal.InOut, true, 0, Number.MAX_VALUE, true);
 
-        this.game.add.sprite(this.game.width * 0.3, this.game.height * 0.7, 'bigCharacterSleeping', 0);
+        this.game.add.sprite(this.game.width * 0.2, this.game.height * 0.7, 'bigCharacterSleeping', 0);
 
         var playGame = this.game.add.button(this.game.width * 0.1, this.game.height * 0.6, 'replay', this.playGame, this);
 
